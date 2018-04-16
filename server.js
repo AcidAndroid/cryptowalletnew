e0e71f7a5e42fab63b28c31111c0e24db490e0a9
kelldintch <kelldintch@hotmail.com>
2018-04-15 20:28:09 -0500

    server parcial

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var http = require('http');
 var cajero = require('./public/js/cajero');
 
+//faltaria agrgar que se reciba por un querysring y que regrese ese valor que se madne en el quiery string
 http.createServer(function(req, res) {
     res.writeHead(200, { 'Content-Type': 'text/json' });
     res.write("dar dinero en: " + JSON.stringify(cajero.entrega([4720])));
